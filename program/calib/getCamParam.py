b5cc4537213245d8d9532ab421ce56a5ceff7ea4
hphnngcquan <haphanngocquan.nd@gmail.com>
2022-09-25 16:01:54 +0700

    feat[sw]: add and test code for finding pattern corners

---
+++ program/calib/getCamParam.py
@@ -0,0 +1,11 @@
+import cv2
+
+vcap = cv2.VideoCapture(0) # 0=camera
+ 
+if vcap.isOpened(): 
+    # get vcap property 
+    print("width", vcap.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
+
+    print("height", vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))  # float `height`
+
+    # fps = vcap.get(cv2.cv.CV_CAP_PROP_FPS)
